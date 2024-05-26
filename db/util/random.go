ec37dac592b3fd9e2a7d0a7c44fedbd6a27fc73c
Aviral <yaviral17@gmail.com>
2024-05-26 19:51:06 +0530

    random value generation

---
+++ db/util/random.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"strings"
+	"time"
+
+	"math/rand"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return int64(RandomInt(0, 1000))
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
